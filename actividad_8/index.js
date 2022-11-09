45e3f5b288f7613d41c76efec509b656e7ec603d
OSCAR JASSIEL ENRIQUEZ LARA <oenriquezl@est.utn.ac.cr>
2022-11-08 22:14:48 -0600

    Actividad 8 lista ya almacena los datos correctamente en el local storage.

---
+++ actividad_8/index.js
@@ -0,0 +1,9 @@
+$("#enviar").click(()=>{
+    let usuario={nombre:$("#nombre").val(),apellido:$("#apellido").val(),telefono:$("#telefono").val()};
+    if (localStorage.length==0) {
+        localStorage.setItem("usuario_1",JSON.stringify(usuario));
+    }else{
+        let ind=parseInt(localStorage.key(0).split("_")[1])+1;
+        localStorage.setItem("usuario_"+String(ind),JSON.stringify(usuario));
+    }
+});
